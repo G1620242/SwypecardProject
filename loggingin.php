3e9497fcd725190454c6ca9dc8a1e4b19758cdcf
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-07 16:55:52 +0000

    pphp

--- loggingin.php
+++ loggingin.php
@@ -5,7 +5,7 @@ session_start();
 ini_set('display_errors', 1);
 $info = "";
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-   // username and password sent from form 
+  
 
    $myusername = mysqli_real_escape_string($conn,$_POST["username"]);
    $mypassword = mysqli_real_escape_string($conn,$_POST["password"]); 
@@ -17,7 +17,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
    $count = mysqli_num_rows($result);
 
-   // If result matched $myusername and $mypassword, table row must be 1 row
+  
 
    if($count == 1) {
       $_SESSION["login_user"] = $row["CustomerID"];
