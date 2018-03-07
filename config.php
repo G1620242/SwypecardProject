7d11128710905b8425547d36e34f3acf14e47c3d
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-07 14:25:32 +0000

    gwiddlehosting

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-   $servername = "";
-   $username = "";
-   $password = "";
-   $dbname = "";
+   $servername = "localhost:3306";
+   $username = "benjamingittus";
+   $password = "password123";
+   $dbname = "SwypeCardDatabase";
    $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
