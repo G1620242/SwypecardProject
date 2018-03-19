5e6068dae6bff86a332aa6e77492831538cbcf7d
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-19 11:51:48 +0000

    comma

--- userdetails.php
+++ userdetails.php
@@ -1,6 +1,6 @@
 <?php
 include('session.php');
-$sql = "SELECT FirstName, LastName, Username, Password, Address, DOB, Email, PhoneNumber, CustomerID FROM user WHERE CustomerID = '$_SESSION[login_user]'";
+$sql = "SELECT FirstName, LastName, Username, Password, Address, DOB, Email, PhoneNumber, CustomerID FROM user WHERE CustomerID = '$_SESSION[login_user]' ";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 mysqli_close($conn);
