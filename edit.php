e3abed1497dba7b3bc66fd601704970deda18bdb
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-19 09:28:42 +0000

    editing details

--- edit.php
+++ edit.php
@@ -1,5 +1,54 @@
 <?php
-include("editingdetails.php");
+include("session.php");
+$info ="";
+function displayUser($conn, $login_user) {
+    $sql = "SELECT FirstName, LastName, Username, Password, Address, DOB, Email, PhoneNumber, FROM user
+    WHERE CustomerID = '$login_user' ";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+    return $row;
+}
+function updateUser($conn, $login_user) {
+    $myfirstname = mysqli_real_escape_string($conn,$_POST["first_name"]);
+    $mylastname = mysqli_real_escape_string($conn,$_POST["last_name"]);
+    $myusername = mysqli_real_escape_string($conn,$_POST["username"]);
+    $myaddress = mysqli_real_escape_string($conn,$_POST["address"]);
+    $myDOB = mysqli_real_escape_string($conn,$_POST["age"]);
+    $myphonenumber = mysqli_real_escape_string($conn,$_POST["phone_number"]);
+
+$sql = "UPDATE user SET FirstName = '$myfirstname', LastName = '$mylastname', Username = '$myusername', Address = '$myaddress', DOB = '$myDOB' PhoneNumber = '$myphonenumber' WHERE CustomerID = '$_SESSION[login_user]' ";
+if (mysqli_query($conn, $sql)) {
+    $info = "Updated ";
+    header("Location: profile.php");
+} else {
+    $info = "Error updating: ". mysqli_error($conn);
+}
+return $info;
+}
+function deleteUser($conn, $login_user) {
+$sql = "DELETE FROM user WHERE CustomerID = '$_SESSION[login_user]' ";
+if (mysqli_query($conn, $sql)) {
+    $info = "Deleted ";
+header("Location: log_out.php");
+} else {
+    $info = "Error deleting: " . mysqli_error($conn);
+}
+return $info;
+}
+if(isset($_POST["update"])){
+    $info = updateUser($conn, $_SESSION["login_user"]);
+    $row = displayUser($conn, $_SESSION["login_user"]);
+}
+else if (isset($_POST["delete"])){
+    $info = deleteUser($conn, $_SESSION["login_user"]);
+}
+else
+{
+    $row = displayUser($conn, $_SESSION["login_user"]);
+}
+mysqli_close($conn);
+
 ?>
 
   <html>
@@ -8,16 +57,16 @@ include("editingdetails.php");
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-          
+
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      
+
       <!--My Style Sheet-->
       <link href="css/myStyleSheet.css" type="text/css" rel="stylesheet"/>
       <link type="text/css" rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css"/>
     </head>
 
     <body>
-        
+
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
@@ -60,14 +109,14 @@ include("editingdetails.php");
              <li><a href="chatroom.php">Support</a></li>
              <li><a href="profile.php">Profile</a></li>
              <li><a href="log_out.php">Log out</a></li>
-             
+
       </ul>
     </div>
   </nav>
         <!--Navbar End-->
         <br>
-        
-           
+
+
         <div class="section">
         <div class="container">
             <form action = "" method = "post">
@@ -91,7 +140,7 @@ include("editingdetails.php");
 </div>
 
 
-        
+
         <footer class="page-footer">
           <div class="container">
             <div class="row">
@@ -111,6 +160,6 @@ include("editingdetails.php");
             </div>
           </div>
         </footer>
-            
+
     </body>
   </html>
