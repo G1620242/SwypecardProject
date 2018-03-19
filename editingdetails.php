fd303f1cd8d6413e5c5caa77835d533eaadf5680
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-19 11:54:57 +0000

    work please

--- editingdetails.php
+++ editingdetails.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
 include("session.php");
 $info ="";
+$login_user = $_SESSION["login_user"];
 function displayUser($conn, $login_user) {
-    $sql = "SELECT FirstName, LastName, Username, Password, Address, DOB, Email, PhoneNumber, FROM user
+    $sql = "SELECT FirstName, LastName, Username, Password, Address, DOB, Email, PhoneNumber FROM user
     WHERE CustomerID = '$login_user' ";
-    $result = mysqli_query($conn,$sql);
-    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0) {
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    } else {
+
+      echo " Not Working Correctly";
+    }
 
     return $row;
 }
@@ -17,7 +24,7 @@ function updateUser($conn, $login_user) {
     $myDOB = mysqli_real_escape_string($conn,$_POST["age"]);
     $myphonenumber = mysqli_real_escape_string($conn,$_POST["phone_number"]);
 
-$sql = "UPDATE user SET FirstName = '$myfirstname', LastName = '$mylastname', Username = '$myusername', Address = '$myaddress', DOB = '$myDOB' PhoneNumber = '$myphonenumber' WHERE CustomerID = '$_SESSION[login_user]' ";
+$sql = "UPDATE user SET FirstName = '$myfirstname', LastName = '$mylastname', Username = '$myusername', Address = '$myaddress', DOB = '$myDOB', PhoneNumber = '$myphonenumber' WHERE CustomerID = '$login_user' ";
 if (mysqli_query($conn, $sql)) {
     $info = "Updated ";
     header("Location: profile.php");
