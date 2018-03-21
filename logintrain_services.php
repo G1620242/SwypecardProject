c690dba9c0da05cdf0e26290678e6dcda93abb5c
Benjamin Gittus <G1620242@student.southstaffs.ac.uk>
2018-03-21 20:12:34 +0000

    navbar 2 fin

--- logintrain_services.php
+++ logintrain_services.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+include("details.php");
+?>
   <html>
     <head>
       <!--Import Google Icon Font-->
@@ -14,50 +16,8 @@
     </head>
 
     <body>
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-      <script> $(document).ready(function(){$('.parallax').parallax();});</script>
-      <script>$( document ).ready(function(){$(".button-collapse").sideNav();})</script>
-      <script>$( document ).ready(function(){$(".dropdown-button").dropdown();})</script>
-        <!--Navbar Start"-->
-   <ul id="dropdown1" class="dropdown-content">
-        <li><a href="loginbus_services.html">Bus</a></li>
-        <li class="divider"></li>
-        <li><a href="train_services.html">Trains</a></li>
-        <li class="divider"></li>
-        <li><a href="taxi_services.html">Taxi</a></li>
-    </ul>
-    <ul id="dropdown2" class="dropdown-content">
-        <li><a href="loginbus_services.html">Bus</a></li>
-        <li class="divider"></li>
-        <li><a href="logintrain_services.html">Trains</a></li>
-        <li class="divider"></li>
-        <li><a href="logintaxi_services.html">Taxi</a></li>
-    </ul>
-    <nav class="nav">
-    <div class="nav-wrapper container">
-      <a href="loginindexpage.php" class="brand-logo"><img src ="assets/SwypeCard%202.jpg"></a>
-    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="loginabout.html">About</a></li>
-            <li><a class = "dropdown-button" href="#" data-activates="dropdown1">Services <i class="material-icons right">arrow_drop_down</i></a></li>
-            <li><a href="loginbooking.html">Book</a></li>
-            <li><a href="loginfaq.html">FAQ</a></li>
-            <li><a href="sign_up.php">Sign Up</a></li>
-             <li><a href="log_in.php">Log In</a></li>
-      </ul>
-         <ul class="side-nav" id="mobile-demo">
-             <li><a href="about.html">About</a></li>
-             <li><a class = "dropdown-button" href="#" data-activates="dropdown2">Services <i class="material-icons right">arrow_drop_down</i></a></li>
-             <li><a href="loginbooking.html">Book</a></li>
-             <li><a href="loginfaq.html">FAQ</a></li>
-             <li><a href="sign_up.php">Sign Up</a></li>
-             <li><a href="log_in.php">Log In</a></li>
-      </ul>
-    </div>
-  </nav>
-        <!--Navbar End-->
+      
+        <?php include("logged_in_navbar.php");?>
 
   <div class="section white">
     <div class="row container">
