5de84d7d1d3b203d0c5b2fba2be122311993c3ea
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-21 14:36:07 +0000

    class

--- chatroom.php
+++ chatroom.php
@@ -5,14 +5,6 @@ include("session.php");
 $sql = "SELECT Username FROM user WHERE CustomerID = '$_SESSION[login_user]'";
 $result = mysqli_query($conn,$sql);
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        $myusername = $row["FirstName"];
-    }
-} else {
-}
 ?>
     <html>
 
@@ -136,7 +128,7 @@ if (mysqli_num_rows($result) > 0) {
                 <form class="col s4" method="post">
                     <div class="row">
                         <div class="input-field col s12">
-                            <input name="author" type="text" class="validate" required value="<?php echo $myusername;?>">
+                            <input name="author" type="text" class="validate" required value="<?php echo $Username;?>">
                             <label for="author">Author</label>
                         </div>
                     </div>
