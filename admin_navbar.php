ba4788937ff994716b9763ded6d684feafa00716
Benjamin Gittus <G1620242@student.southstaffs.ac.uk>
2018-03-21 20:21:08 +0000

    adminnavbar

---
+++ admin_navbar.php
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script>$( document ).ready(function(){$(".button-collapse").sideNav();})</script>
+      <script>$( document ).ready(function(){$(".dropdown-button").dropdown();})</script>
+        <!--Navbar Start"-->
+    <nav class="nav">
+    <div class="nav-wrapper container">
+      <a href="index.php" class="brand-logo"><img src ="assets/SwypeCard%202.jpg"></a>
+    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="newadminpage.php">Create Admin</a></li>
+           
+      </ul>
+         <ul class="side-nav" id="mobile-demo">
+         <li><a href="newadminpage.php">Create Admin</a></li>
+             
+             
+      </ul>
+    </div>
+  </nav>
