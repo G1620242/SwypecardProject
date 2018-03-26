bd91c33f2ee879ea501678cfd552bdf39b09ccd9
G1620242 <G1620242@student.southstaffs.ac.uk>
2018-03-26 11:48:41 +0100

    desc

--- adminbookingviewcolour.php
+++ adminbookingviewcolour.php
@@ -32,7 +32,7 @@ include("admindetails.php");
            </th><th><a href="adminbookingviewdesign">DesignChoice</a></th><th><a href="adminbookingviewstyle">StyleChoice</a></th><th></th></tr>
      <?php
 
-       $sql = "SELECT booking.BookingID, booking.ColourChoice, booking.DesignChoice, booking.StyleChoice, user.Username FROM booking INNER JOIN user ON user.CustomerID = booking.CustomerID ORDER BY ColourChoice DESC";
+       $sql = "SELECT booking.BookingID, booking.ColourChoice, booking.DesignChoice, booking.StyleChoice, user.Username FROM booking INNER JOIN user ON user.CustomerID = booking.CustomerID ORDER BY ColourChoice";
        $res = mysqli_query($conn, $sql);
 
        $bookconfirm = "";
